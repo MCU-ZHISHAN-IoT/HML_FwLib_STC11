c77f9fa7ebc6e0d64ac41bf0f8092049903a5471
WeilunFong <wlf@zhishan-iot.tk>
2022-01-20 01:25:24 +0800

    feat: add CMake support

--- README.md
+++ README.md
@@ -31,36 +31,38 @@ Here is a list of the all on-chip peripheral drivers and examples that need to b
 | UART | universal asynchronous receiver/transmitter | supported |
 | WDT | watchdog | supported |
 
-## Prerequisite
-+ [GNU Make](http://www.gnu.org/software/make/manual/make.html)(recommend)
-+ [SDCC compiler](http://sdcc.sourceforge.net/)
-+ *\[for Windows\]* Unix shell tools([msys](http://www.mingw.org/wiki/MSYS), [Cygwin](http://www.cygwin.com/), [GNUwin32](http://gnuwin32.sourceforge.net/)) needed by makefile for HML_FwLib_STC11
-
-## Usage
-### file structure
-```bash
-HML_FwLib_STC11
-├─doc      #store related documents about HML_FwLib_STC11
-├─example  #provide some example files with *.c format to help users learn about HML_FwLib_STC11
-├─inc      #include all header files(*.h) of HML_FwLib_STC11
-├─obj      #store all output files, including *.hex,*.ihx,*.lk,*.rel and others during compilation
-├─src      #store all source files(*.c) of HML_FwLib_STC11
-├─usr      #store makefile and a source file which includes main function
-├─LICENSE  #license of HML_FwLib_STC11
-└─VERSION  #version code of HML_FwLib_STC11
-```
-### configuration
+## Config
 There are several parameters need to be configured by user manually.
-#### \_\_CONF\_COMPILE\_xxx (for conditional compilation)
+### \_\_CONF\_COMPILE\_xxx (for conditional compilation)
 In order to ensure the projects based on HML_FwLib_STC11 can be downloaded into the limited on-chip flash space of STC11 MCUs, the developers can modify the macro definition named `__CONF_COMPILE_xxx` in *conf.h* to specify which piece of codewill take part in compilation, then it will reduce size of final .hex file. If user only use GPIO module, then user just need to enable `__CONF_COMPILE_GPIO` macro in *conf.h*. Some macros for conditional compilation rely on others. For example, before you define the macro definition `__CONF_COMPILE_RCC`, the macro `__CONF_COMPILE_UTIL` should be defined, otherwise the compilation would be failed.
-####  \_\_CONF\_FRE\_CLKIN
+###  \_\_CONF\_FRE\_CLKIN
 The macro mark frequency of clock source, including extern crystal oscillator or internal RC oscillating circuit, and it's defined in *conf.h*.
-#### \_\_CONF\_MCU\_MODEL
-The macro mark the model of target MCU and is defined in *conf.h*.
-### code & compilation
-There is a source file named *test.c* under *usr* directory, we have put a main function here. User can add and modify own code here, then enter <kbd>make</kbd> in terminal, the Makefile will work and complete compilation. Besides, user can enter <kbd>make help</kbd> to get all usages.
+### \_\_CONF\_HML\_POW
+Enable function pow() which provided by HML. SDCC builtin library doesn't have this function.
+### \_\_CONF\_MCU\_MODEL
+The macro marks the model of target MCU and it's defined in *conf.h*.
 
-Certainly, you can just add *inc* and *src* directory into your project structure, and write your own makefile to build a custom project. 
+## Building
+We provide two kinds of build system support:
+* cmake
+* gmake
+
+### cmake
+Run this command for initialization. Add option `-GNinja` if you expect to build project via Ninja.
+```
+$ cmake -S . -B build
+```
+Run this command for building project. Add option `--clean-first` if you want to execute clean operation before building.
+```
+$ cmake --build build -j$(nproc)
+```
+You can execute command `cmake --build build -t usage` for usage. If you want to adjust build details, please modify `cmake/config.cmake`.
+### gmake
+Execute this command for building HML_FwLib_STC11 with GNU Make.
+```
+$ make -j$(nproc)
+```
+You can execute command `make help` for usage. If you want to adjust build details, please modify `mk/config.mk`.
 
 ## Contributing
 Welcome suggestions and contribution from you! You can fork it or contact us via *[mcu@zhishan-iot.tk](mailto:mcu@zhishan-iot.tk)*.
---
+++ cmake/cflags.cmake
@@ -0,0 +1,173 @@
+# ------------------------------------------------------------------------
+# Author     : Weilun Fong | wlf@zhishan-iot.tk
+# Date       : 2022-01-16
+# Description: project Makefile
+# E-mail     : mcu@zhishan-iot.tk
+# Make-tool  : CMake
+# Page       : https://hw.zhishan-iot.tk/page/hml/detail/fwlib_stc11.html
+# Project    : HML_FwLib_STC11
+# Version    : v0.0.1
+# ------------------------------------------------------------------------
+
+# Init variables
+set(MCU_MODEL "")
+set(CODE_SIZE 0)
+set(XRAM_SIZE 0)
+
+# [CLKFRE] frequency of MCU clock
+if(CLKFRE MATCHES "^[0-9]+$")
+    set(CLOCK_FREQUENCY ${CLKFRE})
+else()
+    message(FATAL_ERROR "invalid CLKFRE value")
+endif()
+
+# [MCU] MCU model
+#   Default value of CODE and XRAM is ruled by MCU model. However, if user
+#   specify them manually, value of related variables will be overridden.
+if(DEFINED MCU)
+    string(TOUPPER ${MCU} MCU_MODEL)
+    if(${MCU_MODEL} STREQUAL STC11F01)
+        set(CODE_SIZE 1024)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F01E)
+        set(CODE_SIZE 1024)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11L01E)
+        set(CODE_SIZE 1024)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F02)
+        set(CODE_SIZE 2048)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11L02)
+        set(CODE_SIZE 2048)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F02E)
+        set(CODE_SIZE 2048)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11L02E)
+        set(CODE_SIZE 2048)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F03E)
+        set(CODE_SIZE 3072)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11L03E)
+        set(CODE_SIZE 3072)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F04)
+        set(CODE_SIZE 4096)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11L04)
+        set(CODE_SIZE 4096)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F04E)
+        set(CODE_SIZE 4096)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11L04E)
+        set(CODE_SIZE 4096)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F05E)
+        set(CODE_SIZE 5120)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11L05E)
+        set(CODE_SIZE 5120)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL IAP11F06)
+        set(CODE_SIZE 6144)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL IAP11L06)
+        set(CODE_SIZE 6144)
+        set(XRAM_SIZE 0)
+    elseif(${MCU_MODEL} STREQUAL STC11F08XE)
+        set(CODE_SIZE 8192)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L08XE)
+        set(CODE_SIZE 8192)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F16XE)
+        set(CODE_SIZE 16384)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L16XE)
+        set(CODE_SIZE 16384)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F32XE)
+        set(CODE_SIZE 32768)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L32XE)
+        set(CODE_SIZE 32768)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F40XE)
+        set(CODE_SIZE 40960)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L40XE)
+        set(CODE_SIZE 40960)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F48XE)
+        set(CODE_SIZE 49152)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L48XE)
+        set(CODE_SIZE 49152)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F52XE)
+        set(CODE_SIZE 53248)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L52XE)
+        set(CODE_SIZE 53248)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F56XE)
+        set(CODE_SIZE 57344)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L56XE)
+        set(CODE_SIZE 57344)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F60XE)
+        set(CODE_SIZE 61440)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L60XE)
+        set(CODE_SIZE 61440)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11F62X)
+        set(CODE_SIZE 63488)
+        set(XRAM_SIZE 1024)
+    elseif(${MCU_MODEL} STREQUAL STC11L62X)
+        set(CODE_SIZE 63488)
+        set(XRAM_SIZE 1024)
+    else()
+        message(FATAL_ERROR "unknow MCU model: ${MCU}")
+    endif()
+else()
+    message(FATAL_ERROR "user must specify MCU model")
+endif()
+
+
+# [CODE] internal ROM
+if(DEFINED CODE)
+    math(EXPR CODE_SIZE "${CODE} * 1024")
+endif()
+
+# [IRAM] internal RAM
+# for stc11, this value is fixed to 256
+set(IRAM_SIZE 256)
+
+# [XRAM] external RAM
+if(DEFINED XRAM)
+    math(EXPR XRAM_SIZE "${XRAM} * 1024")
+endif()
+
+# Compile parameters define
+add_compile_options(
+   --std-sdcc99
+   --fsigned-char
+   --opt-code-size
+   "SHELL: --code-loc 0x0000 --code-size ${CODE_SIZE}"
+   "SHELL: --iram-size ${IRAM_SIZE}"
+   "SHELL: --xram-size ${XRAM_SIZE}"
+)
+add_definitions(
+    -D__CONF_FRE_CLKIN=${CLOCK_FREQUENCY}UL
+    -D__CONF_MCU_MODEL=MCU_MODEL_${MCU_MODEL}
+)
+
+# Print config information
+message("model : ${MCU_MODEL}")
+message("memory: code=${CODE_SIZE}B, iram=${IRAM_SIZE}B, xram=${XRAM_SIZE}B")
+message("clock : ${CLOCK_FREQUENCY}Hz")
